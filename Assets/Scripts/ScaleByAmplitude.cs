2d56112ead9e02070e501deaa4fe7db0d37da29a
SRhodenizer <scr3876@g.rit.edu>
2020-04-16 00:32:40 -0400

    added exploding spheres

---
+++ Assets/Scripts/ScaleByAmplitude.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleByAmplitude : MonoBehaviour
+{
+
+    private Material mat;
+    public int band;
+    public float startScale, maxScale;
+    public bool useBuffer;
+
+    public float red, green, blue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3((AudioPeer.amplitude * maxScale)+startScale, (AudioPeer.amplitude * maxScale) + startScale, (AudioPeer.amplitude * maxScale) + startScale);
+
+            Color color = new Color(red * AudioPeer.amplitude, green*AudioPeer.amplitude, blue*AudioPeer.amplitude);
+
+            mat.SetColor("_EmissionColor", color);
+        }
+
+        if (!useBuffer)
+        {
+            transform.localScale = new Vector3((AudioPeer.amplitudeBuffer * maxScale) + startScale, (AudioPeer.amplitudeBuffer * maxScale) + startScale, (AudioPeer.amplitudeBuffer * maxScale) + startScale);
+
+            Color color = new Color(red * AudioPeer.amplitudeBuffer, green * AudioPeer.amplitudeBuffer, blue * AudioPeer.amplitudeBuffer);
+
+            mat.SetColor("_EmissionColor", color);
+        }
+    }
+}
